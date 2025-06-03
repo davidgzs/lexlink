60c408dce5144890de2b54776928cb2f9f211319
David González Sáiz <DavidGZs@gmail.com>
2025-06-03 08:42:36 +0000

    cambia el texto de LexLINK conecta despachos de abogados con sus clientes en tiempo real. Gestiona citas, sigue el progreso de casos y comunícate de forma segura, todo en un solo lugar. por este otro LexLINK conecta a los despachos de abogados con sus clientes en tiempo real de manera moderna y eficiente. Gestiona las citas, el progreso de las casos y expedientes. Comunicandose todo de forma segura, todo en un solo lugar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function LandingPage() {
               Revolucionando la Comunicación de los despachos de abogados con sus clientes y sus asuntos legales ("sus casos")
             </h2>
             <p className="text-lg md:text-xl text-foreground mb-8 max-w-3xl mx-auto">
-              LexLINK conecta despachos de abogados con sus clientes en tiempo real. Gestiona citas, sigue el progreso de casos y comunícate de forma segura, todo en un solo lugar.
+              LexLINK conecta a los despachos de abogados con sus clientes en tiempo real de manera moderna y eficiente. Gestiona las citas, el progreso de las casos y expedientes. Comunicandose todo de forma segura, todo en un solo lugar.
             </p>
             <Link href="/login">
               <Button size="lg" className="font-body">
