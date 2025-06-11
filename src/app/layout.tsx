31d074b1a19143a4d5c596b6c2ca5bcdfb31239c
David González Sáiz <DavidGZs@gmail.com>
2025-06-11 19:52:08 +0000

    tengo parece ser un error aqui src/app/layout.tsx (23:7) @ RootLayout Hy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import ClientOnly from '@/components/layout/ClientOnly';
 
 export const metadata: Metadata = {
   title: 'LexLINK',
@@ -22,7 +24,9 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
-        <Toaster />
+        <ClientOnly>
+          <Toaster />
+        </ClientOnly>
       </body>
     </html>
   );
